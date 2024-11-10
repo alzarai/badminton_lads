2bb57b8090aec9848c29549670f91b35f38ec7bf
MohamedMbarouk <mmm1070@nyu.edu>
2024-11-11 00:24:20 +0400

    setting up the structure of the predictor codeblock

---
+++ match_predictor.py
@@ -0,0 +1,16 @@
+#The code that will be used to build and train a NN to see if we can we predict the outcome of matches
+#We will assume we are given the files that have been created as they are - hence we will need to combine into one master file and extract relevant data to be fed into model
+
+
+
+#Defining the function that will generate the training data list form the files
+#def define_training_data():
+
+#Defining the function that will generate the training labels list form the files
+#def define_training_list():
+
+#Defining the function that will build the neural network
+#def predictor():
+
+#Defining the function that will determine the accuracy of our model, by comparing its evaluation against a new set of player data that will be created using the original formula
+#def accuracy_determinator():
