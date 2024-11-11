ba20389284f310d7810d9589515f9b5693738f77
MohamedMbarouk <mmm1070@nyu.edu>
2024-11-11 21:36:31 +0400

    basic creation of the neural network.

--- match_predictor.py
+++ match_predictor.py
@@ -1,6 +1,11 @@
 from os import path
 import numpy as np
 import csv 
+import tensorflow
+from tensorflow import keras
+
+
+#from tensorflow.keras import Sequential
 
 #The code that will be used to build and train a NN to see if we can we predict the outcome of matches
 #We will assume we are given the files that have been created as they are - hence we will need to combine into one master file and extract relevant data to be fed into model
@@ -20,13 +25,10 @@ def define_training_data():
         reader = list(reader)
     for i in range(len(reader)):
         for j in range(len(reader)):  # Pair with all rows from i to the last row
-            combined_row = reader[i][1:] + reader[j][1:]  # Skip the first column of each row
+            combined_row = reader[i][2:] + reader[j][2:]  # Skip the first column of each row
             x_data.append(combined_row)
     x_data = np.array(x_data)   
-    print(len(x_data))
-    print(x_data[0],x_data[1],x_data[-1])
-define_training_data()
-
+    return(x_data)
 
 #Defining the function that will generate the training labels list form the files
 def define_training_list():
@@ -40,11 +42,26 @@ def define_training_list():
         for row in reader:
             y_row = float(row[2:][0])
             y_data.append(y_row)
-        print(len(y_data)) 
-#define_training_list()
+    return(y_data)
+
 
 #Defining the function that will build the neural network
-#def predictor():
+def predictor():
+    #Getting training data and labels
+    x_data = define_training_data()
+    y_data = define_training_list()
+    #Creating the NN
+    model = keras.Sequential([
+        keras.layers.Input(shape=(len(x_data),)),
+        keras.layers.Dense(25,activation='relu'),
+        keras.layers.Dense(15,activation='relu'),
+        keras.layers.Dense(1,activation='linear')
+    ])
+    model.compile(optimizer='adam',loss='mse')
+    model.fit(x_data,y_data,epochs=100)
+
+predictor()
+
 
 #Defining the function that will determine the accuracy of our model, by comparing its evaluation against a new set of player data that will be created using the original formula
 #def accuracy_determinator():
