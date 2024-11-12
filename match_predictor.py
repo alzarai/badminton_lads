d8351bd589dc285ea6c65af29ad727a2a5fd8c79
MohamedMbarouk <mmm1070@nyu.edu>
2024-11-12 23:59:53 +0400

    NN built and callback defined. Comparison against unseen data shows a % error of 1.77.

--- match_predictor.py
+++ match_predictor.py
@@ -1,16 +1,12 @@
 from os import path
 import numpy as np
 import csv 
-import tensorflow
+import tensorflow as tf
 from tensorflow import keras
-
-
-#from tensorflow.keras import Sequential
+tf.random.set_seed(42)
 
 #The code that will be used to build and train a NN to see if we can we predict the outcome of matches
 #We will assume we are given the files that have been created as they are - hence we will need to combine into one master file and extract relevant data to be fed into model
-
-
 
 #Defining the function that will generate the training data list form the files
 #For simplicity we will assume that the order of the training data is the same as the label data
@@ -46,7 +42,6 @@ def define_training_data():
     #converting all training values to float to be fed into the neural network
     x_data = x_data.astype(float)
     return(x_data)
-define_training_data()
 
 #Defining the function that will generate the training labels list form the files
 def define_training_list():
@@ -64,11 +59,10 @@ def define_training_list():
 
 
 #Defining the function that will build the neural network
-def predictor():
-    #Getting training data and labels
-    x_data = np.array(define_training_data())
-    y_data = np.array(define_training_list())
-    print(x_data.shape)
+def predictor(x_data,y_data):
+    #Tiling the data to get more out of it
+    x_data = np.tile(x_data,(100,1))
+    y_data= np.tile(y_data.reshape(-1,1),(100,1))
     #Creating the NN
     model = keras.Sequential([
         keras.layers.Input(shape=(x_data.shape[-1],)),
@@ -77,11 +71,32 @@ def predictor():
         keras.layers.Dense(1,activation='linear')
     ])
     model.compile(optimizer='adam',loss='mse')
-    model.fit(x_data,y_data,epochs=100)
+
+    # Define the EarlyStopping callback
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor='loss',          # Metric to monitor (can also use 'val_loss' if validating)
+        min_delta=0.000002,         # Minimum change to qualify as an improvement
+        patience=1,              # Number of epochs with no improvement before stopping
+        mode='min',              # Stop when the monitored metric has stopped decreasing
+        baseline=0.000001,           # Set your target loss threshold here
+        restore_best_weights=True # Optionally restore the best weights after stopping
+    )
+
+    model.fit(x_data,y_data,epochs=100,callbacks=[early_stopping])
     return(model)
 
-my_first_model = predictor()
-my_first_model.summary()
+#Getting training data and labels
+x_data = np.array(define_training_data())
+x_test = x_data[-1:]
+x_data = x_data[:-1]
+y_data = np.array(define_training_list())
+y_test = y_data[-1:]
+y_data = y_data[:-1]
+my_first_model = predictor(x_data,y_data)
+
+#Seeing how well my model can predict on data it's never seen before
+y_predict = my_first_model.predict(x_test)
+print(f'What I predicted was {y_predict} and the true value was {y_test}, meaning a percentage difference of {np.abs((y_predict-y_test)/y_test)*100}')
 
 #Defining the function that will determine the accuracy of our model, by comparing its evaluation against a new set of player data that will be created using the original formula
 #def accuracy_determinator():
