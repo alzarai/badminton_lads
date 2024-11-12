073f6aa4e0440b770272bf3e7fd08d8d584b0b56
MohamedMbarouk <mmm1070@nyu.edu>
2024-11-12 20:30:59 +0400

    fixed the loading of the training data to ensure all values read from file are associated to numeric value and converted to float

--- match_predictor.py
+++ match_predictor.py
@@ -25,10 +25,28 @@ def define_training_data():
         reader = list(reader)
     for i in range(len(reader)):
         for j in range(len(reader)):  # Pair with all rows from i to the last row
-            combined_row = reader[i][2:] + reader[j][2:]  # Skip the first column of each row
+            #First data point is 
+            row_1 = reader[i][2:]
+            #We also know that the last value is always going to be the eyesight, so let us evaluate what that gives us in float form
+            row_1[-1]=eval(row_1[-1])
+            #Second data point is
+            row_2 = reader[j][2:]
+            #We also know that the last value is always going to be the eyesight, so let us evaluate what that gives us in float form
+            row_2[-1]=eval(row_2[-1])
+            #The combined rows that will be used as the total first training example
+            combined_row = row_1+row_2 
             x_data.append(combined_row)
-    x_data = np.array(x_data)   
-    return(x_data)
+    x_data = np.array(x_data) 
+    #We know that all the data in the 5th column is dominant hand and 6th column is gender and we we want to replace these with binary values instead, so we create targets and replacements
+    target_values = ['left','right','male','female']
+    replacement_values = [-1,1,-1,1]
+    for target,replacement in zip(target_values,replacement_values):
+        x_data[x_data == target] = replacement
+
+    #converting all training values to float to be fed into the neural network
+    x_data = x_data.astype(float)
+    #return(x_data)
+define_training_data()
 
 #Defining the function that will generate the training labels list form the files
 def define_training_list():
@@ -48,8 +66,8 @@ def define_training_list():
 #Defining the function that will build the neural network
 def predictor():
     #Getting training data and labels
-    x_data = define_training_data()
-    y_data = define_training_list()
+    x_data = np.array(define_training_data())
+    y_data = np.array(define_training_list())
     #Creating the NN
     model = keras.Sequential([
         keras.layers.Input(shape=(len(x_data),)),
